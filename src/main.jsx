da85204204fce4bff91b9e4614fafaa062d97c83
sniiz <88880069+sniiz@users.noreply.github.com>
2024-02-04 22:21:01 +0300

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { inject } from "@vercel/analytics";
+
+inject({
+  mode: window.location.hostname === "localhost" ? "development" : "production",
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
