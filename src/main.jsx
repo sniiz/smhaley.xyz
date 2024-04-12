2123567105fecbb695e9c2ba863ddfe22a74fdae
sniiz <88880069+sniiz@users.noreply.github.com>
2024-04-12 16:44:50 +0300

    forgot the analytics woops

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./routes/home";
 
+import { inject } from "@vercel/analytics";
+
+inject({
+  mode: window.location.hostname === "localhost" ? "development" : "production",
+});
+
 const routes = [{ path: "/", element: <Home /> }];
 
 const router = createBrowserRouter(routes);
